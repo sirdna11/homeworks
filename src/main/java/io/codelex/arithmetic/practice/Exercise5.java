d80c03ba444fb4eea089408e678e4912e5b18847
sirdna11 <88270051+sirdna11@users.noreply.github.com>
2023-05-10 15:35:31 +0300

    Update Exercise5.java
    formated code

--- src/main/java/io/codelex/arithmetic/practice/Exercise5.java
+++ src/main/java/io/codelex/arithmetic/practice/Exercise5.java
@@ -1,21 +1,26 @@
 package io.codelex.arithmetic.practice;
+
 import java.util.Scanner;
 import java.util.Random;
+
 public class Exercise5 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
+        
         int randomNumber = random.nextInt(100) + 1;
+        
         System.out.println(randomNumber);
-        System.out.println("I'm thinking of a number between 1-100.  Try to guess it.");
+        System.out.println("I'm thinking of a number between 1-100. Try to guess it.");
         int playersNumber = scanner.nextInt();
 
-        if (playersNumber == randomNumber){
-            System.out.println("You guessed it!  What are the odds?!?");
-        }else if (playersNumber > randomNumber){
-            System.out.println("Sorry, you are too high.  I was thinking of "+randomNumber);
-        }else{
-            System.out.println("Sorry, you are too low.  I was thinking of "+randomNumber);
+        if (playersNumber == randomNumber) {
+            System.out.println("You guessed it! What are the odds?!?");
+        } else if (playersNumber > randomNumber) {
+            System.out.println("Sorry, you are too high. I was thinking of " + randomNumber);
+        } else {
+            System.out.println("Sorry, you are too low. I was thinking of " + randomNumber);
         }
     }
 }
+
