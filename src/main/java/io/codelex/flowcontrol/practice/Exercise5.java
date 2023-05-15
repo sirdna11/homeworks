c22528227a2d64db8a6b37d4052624c59e5326a8
sirdna11 <88270051+sirdna11@users.noreply.github.com>
2023-05-15 15:52:22 +0300

    Update Exercise5.java
    switched to the new switch syntax

--- src/main/java/io/codelex/flowcontrol/practice/Exercise5.java
+++ src/main/java/io/codelex/flowcontrol/practice/Exercise5.java
@@ -6,61 +6,28 @@ public class Exercise5 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-
         String input;
         while (true) {
             System.out.print("Enter a character: ");
             input = in.nextLine().toLowerCase();
-            switch (input) {
-                case "a":
-                case "b":
-                case "c":
-                    System.out.println(2);
-                    break;
-                case "d":
-                case "e":
-                case "f":
-                    System.out.println(3);
-                    break;
-                case "g":
-                case "h":
-                case "i":
-                    System.out.println(4);
-                    break;
-                case "j":
-                case "k":
-                case "l":
-                    System.out.println(5);
-                    break;
-                case "m":
-                case "n":
-                case "o":
-                    System.out.println(6);
-                    break;
-                case "p":
-                case "q":
-                case "r":
-                case "s":
-                    System.out.println(7);
-                    break;
-                case "t":
-                case "u":
-                case "v":
-                    System.out.println(8);
-                    break;
-                case "w":
-                case "x":
-                case "y":
-                case "z":
-                    System.out.println(9);
-                    break;
-                default:
-                    System.out.println("loooool");
+
+            int output = switch (input) {
+                case "a", "b", "c" -> 2;
+                case "d", "e", "f" -> 3;
+                case "g", "h", "i" -> 4;
+                case "j", "k", "l" -> 5;
+                case "m", "n", "o" -> 6;
+                case "p", "q", "r", "s" -> 7;
+                case "t", "u", "v" -> 8;
+                case "w", "x", "y", "z" -> 9;
+                default -> -1;
+            };
+
+            if (output == -1) {
+                System.out.println("loooool");
+            } else {
+                System.out.println(output);
             }
         }
-
-
     }
-
-
 }
